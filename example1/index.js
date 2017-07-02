3df933167a8e3a57993deef4623443bb75894468
Chris Szymansky <chris.szymansky@gmail.com>
2017-07-02 16:48:09 -0400

    Example TestRPC webapp

---
+++ example1/index.js
@@ -0,0 +1,28 @@
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+abi = JSON.parse('[{"constant":false,"inputs":[{"name":"token","type":"bytes32"}],"name":"castVote","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"token","type":"bytes32"}],"name":"totalVotesByToken","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"tokenList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"totalVotes","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"token","type":"bytes32"}],"name":"tokenValidator","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[{"name":"tokenNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]');
+
+ExampleContract = web3.eth.contract(abi);
+
+// Replace this with the value from your Node console > contractInstance.address
+exampleContractInstance = ExampleContract.at('0x8946bac96fd5e35f3665da80bc2dd47f6600e391');
+
+var tokens = document.querySelectorAll(".token");
+
+tokens.forEach(function(token){
+    let tokenName = token.querySelectorAll(".token-name")[0].innerHTML;
+    let contractVotes = exampleContractInstance.totalVotesByToken.call(tokenName).toString();
+
+    token.querySelectorAll(".token-votes")[0].innerHTML = contractVotes;
+});
+
+function castVote(element) {
+    let voteItem = element.getAttribute("data-id");
+    let voteToken = document.getElementById("token-" + voteItem);
+    let voteTokenName = voteToken.querySelectorAll(".token-name")[0].innerHTML;
+
+    exampleContractInstance.castVote(voteTokenName, {from: web3.eth.accounts[0]}, function() {
+        let updatedVotes = exampleContractInstance.totalVotesByToken.call(voteTokenName).toString();
+
+        voteToken.querySelectorAll(".token-votes")[0].innerHTML = updatedVotes;
+    });
+}
